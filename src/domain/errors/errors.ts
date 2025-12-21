d0f1fb4d7805685f1f67890bdff637552a7f4a76
Imasha Perera <imashaperera09@gmail.com>
2025-12-21 22:07:21 +0530

    changes

--- src/domain/errors/errors.ts
+++ src/domain/errors/errors.ts
@@ -16,5 +16,12 @@ export class UnauthorizedError extends Error {
   constructor(message: string) {
     super(message);
     this.name = "UnauthorizedError";
-  } 
+  }
+}
+
+export class ForbiddenError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "ForbiddenError";
+  }
 }
